67ae76ce5c0f1990eeea7cf742fd1bee7a6d8d90
BalconyJH <balconyjh@gmail.com>
2023-12-07 00:23:27 +0800

    :fire: 添加图片预处理函数

--- src/utils/face_classifier.py
+++ src/utils/face_classifier.py
@@ -1,3 +1,5 @@
+
+import cv2
 import dlib
 
 
@@ -5,11 +7,24 @@ class FaceClassifier:
     def __init__(self):
         pass
 
-    async def image_preprocessing(self, image):
-        # use dlib to detect face and crop it
-        dlib.get_frontal_face_detector()
+    @staticmethod
+    async def image_preprocessing(path: str):
+        face_detector = dlib.get_frontal_face_detector()
+        raw_image = cv2.imread(path)
+        gray_image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2GRAY)
+        detected_faces = face_detector(gray_image, 1)
+        cropped_faces = []
 
+        for i, face_rect in enumerate(detected_faces):
+            x1, y1, x2, y2 = face_rect.left(), face_rect.top(), face_rect.right(), face_rect.bottom()
+            crop = gray_image[y1:y2, x1:x2]
+            cropped_faces.append(crop)
 
+        return cropped_faces
+
+    @staticmethod
+    async def save_face_classifier():
+        pass
 
     async def train_face_classifier(self):
         pass
