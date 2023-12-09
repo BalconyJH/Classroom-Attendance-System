ffcde6fda56b8769a206b542966ea203b5928282
BalconyJH <balconyjh@gmail.com>
2023-12-09 13:22:12 +0800

    :boom: 重写log类，遵循单例模式

--- src/utils/log.py
+++ src/utils/log.py
@@ -7,14 +7,47 @@ import loguru
 from src.config import config
 
 if TYPE_CHECKING:
-    from loguru import Logger
+    from loguru import Logger  # noqa F401
 
-logger: "Logger" = loguru.logger
+
+class SingletonLogger:
+    _instance = None
+
+    @classmethod
+    def get_instance(cls):
+        if cls._instance is None:
+            cls._instance = cls()
+        return cls._instance
+
+    def __init__(self):
+        self.logger = loguru.logger
+        self._configure_logger()
+        self._configure_sqlalchemy_logger()
+
+    def _configure_logger(self):
+        default_format = (
+            "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
+            "<level>{level}</level> | "
+            "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
+            "<level>{message}</level>"
+        )
+        self.logger.remove()
+        self.logger.add(
+            sys.stdout,
+            level=config.log_level,
+            format=default_format,
+        )
+
+    @staticmethod
+    def _configure_sqlalchemy_logger():
+        sqlalchemy_logger = logging.getLogger("sqlalchemy.engine")
+        sqlalchemy_logger.addHandler(LoguruHandler())
+        sqlalchemy_logger.setLevel(config.log_level)
 
 
 class LoguruHandler(logging.Handler):
-
     def emit(self, record: logging.LogRecord):
+        logger = SingletonLogger.get_instance().logger
         try:
             level = logger.level(record.levelname).name
         except ValueError:
@@ -28,21 +61,3 @@ class LoguruHandler(logging.Handler):
         logger.opt(depth=depth, exception=record.exc_info).log(
             level, record.getMessage()
         )
-
-
-default_format = (
-    "<green>{time:YYYY-MM-DD HH:mm:ss}</green> | "
-    "<level>{level}</level> | "
-    "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - "
-    "<level>{message}</level>"
-)
-logger.remove()
-logger_id = logger.add(
-    sys.stdout,
-    level=config.log_level,
-    format=default_format,
-)
-if config.log_level == "DEBUG":
-    sqlalchemy_logger = logging.getLogger("sqlalchemy.engine")
-    sqlalchemy_logger.addHandler(LoguruHandler())
-    sqlalchemy_logger.setLevel(config.log_level)
