ea670f448e98054fd5b28d38f6695510944516d3
BalconyJH <balconyjh@gmail.com>
2023-12-09 13:24:51 +0800

    :wrench: 添加模型参数和静态资源参数, 搬移db_model.py

--- src/config.py
+++ src/config.py
@@ -1,9 +1,15 @@
+from pathlib import Path
+
 from pydantic_settings import BaseSettings
 
 
 class Config(BaseSettings):
     log_level: str = "INFO"
-    database_url: str = "sqlite+aiosqlite:///files/data.db"
+    database_url: str = "sqlite+aiosqlite:///files/data.sqlite3"
+    static_path: Path = Path(__file__).parent / "resources" / "statics"
+    predictor_model_path: Path = Path(__file__).parent / "resources" / "models"
+    translation_path: Path = Path(__file__).parent / "resources" / "translations"
+    language_type: str = "zh_CN"
 
     class Config:
         env_file = ".env"
