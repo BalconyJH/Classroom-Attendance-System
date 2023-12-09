34147efa00b2a1d06054c7c7f89040dd91a4eb15
BalconyJH <balconyjh@gmail.com>
2023-12-09 13:19:28 +0800

    :dizzy: 添加翻译模块

---
+++ src/utils/translator.py
@@ -0,0 +1,54 @@
+import json
+from pathlib import Path
+
+from src.config import config
+
+TRANSLATIONS_PATH = Path(f"{config.translation_path}/{config.language_type}.json")
+
+
+class Translator:
+    """
+    消息翻译组件。
+    """
+
+    def __init__(self):
+        from src.utils import logger
+        """
+        加载指定语言的翻译文件并初始化翻译器。
+        """
+        try:
+            with open(TRANSLATIONS_PATH, encoding="utf-8") as f:
+                self.translations = json.load(f)
+        except FileNotFoundError as e:
+            logger.error(f"translation file not found: {e}")
+
+    def extract_value(self, key_sequence):
+        """
+        从给定的JSON数据中，根据键序列抽取值
+        :param self: 输入的JSON数据
+        :param key_sequence: 键序列，例如"commands.login.NAME"
+        :return: 如果找到，则返回对应的值，否则返回None
+        """
+
+        keys = key_sequence.split(".")
+        data = self.translations
+
+        for key in keys:
+            if key in data:
+                data = data[key]
+            else:
+                raise ValueError(f"找不到键{key_sequence}的值")
+
+        return data
+
+    def get_local_translation(self, message_keys):
+        """
+        获取指定消息键的翻译文本。
+        """
+        return self.extract_value(message_keys)
+
+    def get_local_translations(self, *message_keys):
+        """
+        获取多个消息键的翻译文本。
+        """
+        return {key: self.get_local_translation(key) for key in message_keys}
