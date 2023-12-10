10602f3eba9f45b9a9ee816d66627f5a0369c3cc
BalconyJH <balconyjh@gmail.com>
2023-12-10 21:15:55 +0800

    :wrench: 添加模型路径配置项

--- src/config.py
+++ src/config.py
@@ -5,9 +5,10 @@ from pydantic_settings import BaseSettings
 
 class Config(BaseSettings):
     log_level: str = "INFO"
-    database_url: str = "sqlite+aiosqlite:///files/data.sqlite3"
+    database_url: str = "sqlite+aiosqlite:///resources/data/data.sqlite3"
+    cache_path: Path = Path(__file__).parent / "resources" / "caches"
     static_path: Path = Path(__file__).parent / "resources" / "statics"
-    predictor_model_path: Path = Path(__file__).parent / "resources" / "models"
+    face_model_path: Path = Path(__file__).parent / "resources" / "models"
     translation_path: Path = Path(__file__).parent / "resources" / "translations"
     language_type: str = "zh_CN"
 
