68fbe61e5b0d0d051f9cad89bcd2ef0524064119
BalconyJH <balconyjh@gmail.com>
2024-01-11 23:32:37 +0800

    :bug: 修复单元测试

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,11 +1,9 @@
 import pytest
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from src.database.db_model import Base
 
 
-# 创建异步测试数据库引擎
 @pytest.fixture
 async def async_engine():
     engine = create_async_engine("sqlite+aiosqlite:///:memory:", echo=True)
@@ -15,9 +13,10 @@ async def async_engine():
     await engine.dispose()
 
 
-# 异步会话工厂
 @pytest.fixture
-async def async_session(async_engine, async_session):
-    async_session_factory = sessionmaker(async_engine, class_=AsyncSession, expire_on_commit=False)
-    async with async_session_factory() as async_session:
-        yield async_session
+async def async_session(async_engine):
+    async for engine in async_engine:
+        async_session_factory = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+        async with async_session_factory() as session:
+            yield session
+
--- src/tests/test_create_user_and_class.py
+++ src/tests/test_create_user_and_class.py
@@ -1,4 +1,3 @@
-
 import pytest
 
 from src.database.db_model import User
@@ -6,12 +5,8 @@ from src.database.db_model import User
 
 @pytest.mark.asyncio
 async def test_create_user_and_class(async_session):
-    # 创建一个新用户
-    new_user = await User.create(async_session, name="John Doe", student_id="123", password="password")
-    assert new_user is not None
-
-    found_user = await User.read(async_session, new_user.id)
-    assert found_user is not None
-
-    # 清理：删除测试数据（可选）
-    await new_user.delete(async_session)
+    # 使用 async_session 实例进行数据库操作
+    async for session in async_session:
+        # 这里使用 session 进行数据库操作
+        await User.create(session, name="John Doe", student_id="123", password="password")
+        # 其他需要的测试或断言
