9fcac5c1c9e5e7bd7f498c85ca42cb8ee710ca8e
BalconyJH <balconyjh@gmail.com>
2024-01-13 23:57:19 +0800

    :bug: 修复异步pytest引擎引入的神秘bug

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,30 +1,42 @@
 import pytest
-from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
 from utils import logger
-from src.database.db_model import Base
+from database.db_model import Base
+
+# @pytest.fixture
+# async def async_engine():
+#     """
+#     创建一个内存中的 sqlite 数据库引擎。
+#     引擎会在测试结束后自动销毁。
+#     引擎启动时会自动创建所有模型对应的表。
+#     :return: AsyncEngine 实例。
+#     """
+#     engine = create_async_engine("sqlite+aiosqlite:///:memory:", echo=False, future=True)
+#     async with engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.create_all)
+#         logger.info("Create all tables")
+#     yield engine
+#     await engine.dispose()
+#
+#
+# @pytest.fixture
+# async def async_session(async_engine):
+#     async for engine in async_engine:
+#         async_session_factory = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+#         async with async_session_factory() as session:
+#             yield session
 
 
 @pytest.fixture
-async def async_engine():
-    """
-    创建一个内存中的 sqlite 数据库引擎。
-    引擎会在测试结束后自动销毁。
-    引擎启动时会自动创建所有模型对应的表。
-    :return: AsyncEngine 实例。
-    """
-    engine = create_async_engine("sqlite+aiosqlite:///:memory:", echo=False, future=True)
-    async with engine.begin() as conn:
+async def async_session(request):
+    async_engine = create_async_engine("sqlite+aiosqlite:///:memory:", echo=False, future=True)
+
+    async with async_engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
-        logger.info("Create all tables")
-    yield engine
-    await engine.dispose()
 
+    async with AsyncSession(async_engine) as session:
+        yield session
 
-@pytest.fixture
-async def async_session(async_engine):
-    async for engine in async_engine:
-        async_session_factory = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
-        async with async_session_factory() as session:
-            yield session
-
+    await async_engine.dispose()
+    logger.info("Dispose engine")
--- src/tests/test_create_user_and_class.py
+++ src/tests/test_create_user_and_class.py
@@ -5,8 +5,6 @@ from src.database.db_model import User
 
 @pytest.mark.asyncio
 async def test_create_user_and_class(async_session):
-    # 使用 async_session 实例进行数据库操作
-    async for session in async_session:
-        # 这里使用 session 进行数据库操作
-        await User.create(session, name="John Doe", student_id="123", password="password")
-        # 其他需要的测试或断言
+    user = User(name="John Doe", student_id="123", password="password")
+    async_session.add(user)
+    await async_session.commit()
