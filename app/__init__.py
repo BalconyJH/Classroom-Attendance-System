a31fe49d8f3bf1b49fbca684135afd3a2fa6ab6e
BalconyJH <balconyjh@gmail.com>
2024-01-19 23:50:05 +0800

    :tada: 初始化webapp

---
+++ app/__init__.py
@@ -0,0 +1,136 @@
+from logging.config import dictConfig
+from datetime import datetime, timedelta
+
+import dlib
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, flash, request, session, url_for, redirect, render_template
+
+app = Flask(__name__)
+app.config.from_object("config")
+app.secret_key = "123456"
+app.jinja_env.auto_reload = True
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = timedelta(seconds=1)
+db = SQLAlchemy(app)
+from .models import Student, Teacher  # noqa E402
+
+# handler = logging.StreamHandler()
+# formatter = logging.Formatter(
+#     '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+#     datefmt='%d/%b/%Y %H:%M:%S'
+# )
+# handler.setFormatter(formatter)
+# if app.logger.hasHandlers():
+#     app.logger.handlers.clear()
+# app.logger.addHandler(handler)
+print(f"CUDA STATUS:{dlib.DLIB_USE_CUDA}")
+dictConfig({
+    "version": 1,
+    "formatters": {"default": {
+        "format": "[%(asctime)s] %(levelname)s in %(module)s: %(message)s",
+    }},
+    "handlers": {"wsgi": {
+        "class": "logging.StreamHandler",
+        "stream": "ext://flask.logging.wsgi_errors_stream",
+        "formatter": "default"
+    }},
+    "root": {
+        "level": "INFO",
+        "handlers": ["wsgi"]
+    }
+})
+
+
+@app.route("/", methods=["GET", "POST"])
+def login():
+    app.logger.debug("A value for debugging")
+    app.logger.info("Info level log")
+    app.logger.warning("A warning occurred (%d apples)", 42)
+    app.logger.error("An error occurred")
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        if len(username) == 13:
+            students = Student.query.filter(Student.s_id == username).first()
+            if students:
+                if students.s_password == password:
+                    flash("登陆成功")
+                    session["username"] = username
+                    session["id"] = students.s_id
+                    session["num"] = 0  # students.num
+                    session["name"] = students.s_name
+                    session["role"] = "student"
+                    session["flag"] = students.flag
+                    if students.before:
+                        session["time"] = students.before
+                    else:
+                        session["time"] = time
+                    students.before = time
+                    db.session.commit()
+                    return redirect(url_for("student.home"))
+                else:
+                    flash("密码错误，请重试")
+            else:
+                flash("学号错误，请重试")
+        elif len(username) == 8:
+            teachers = Teacher.query.filter(Teacher.t_id == username).first()
+            if teachers:
+                if teachers.t_password == password:
+                    flash("登陆成功")
+                    session["username"] = username
+                    session["id"] = teachers.t_id
+                    session["name"] = teachers.t_name
+                    session["role"] = "teacher"
+                    session["attend"] = []
+                    if teachers.before:
+                        session["time"] = teachers.before
+                    else:
+                        session["time"] = time
+                    teachers.before = time
+                    db.session.commit()
+                    return redirect(url_for("teacher.home"))
+                else:
+                    flash("密码错误，请重试")
+            else:
+                flash("工号错误，请重试")
+        else:
+            flash("账号不合法，请用学号/工号登录")
+    return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    # students = Student.query.filter(Student.s_id == session['id']).first()
+    # students.num = session['num']
+    # db.session.commit()
+    session.clear()
+    return render_template("login.html")
+
+
+# 拦截器
+@app.before_request
+def before():
+    list = ["png", "css", "js", "ico", "xlsx", "xls", "jpg"]
+    url_after = request.url.split(".")[-1]
+    if url_after in list:
+        return None
+    url = str(request.endpoint)
+    if url == "logout":
+        return None
+    if url == "login":
+        if "username" in session:
+            return redirect("logout")
+        else:
+            return None
+    else:
+        if "username" in session:
+            role = url.split(".")[0]
+            if role == session["role"]:
+                return None
+            else:
+                new_endpoint = session["role"] + "." + "home"
+                flash("权限不足")
+                return redirect(url_for(new_endpoint))
+        else:
+            flash("未登录")
+            return redirect("/")
---
+++ app/models.py
@@ -0,0 +1,100 @@
+from app import db  # db是在app/__init__.py生成的关联后的SQLAlchemy实例
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(320), unique=True)
+    password = db.Column(db.String(32), nullable=False)
+
+    def __repr__(self):
+        return "<User %r>" % self.username
+
+
+class Admin(db.Model):
+    __tablename__ = "admins"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(320), unique=True)
+    password = db.Column(db.String(32), nullable=False)
+
+    def __repr__(self):
+        return "<User %r>" % self.username
+
+class Student(db.Model):
+    __tablename__ = "students"
+    s_id= db.Column(db.String(13), primary_key=True)
+    s_name = db.Column(db.String(80), nullable=False)
+    s_password = db.Column(db.String(32), nullable=False)
+    flag = db.Column(db.Integer, default=1)
+    before = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return f"<Student {self.s_id!r},{self.s_name!r}>"
+
+class Teacher(db.Model):
+    __tablename__ = "teachers"
+    t_id= db.Column(db.String(8), primary_key=True)
+    t_name = db.Column(db.String(80), nullable=False)
+    t_password = db.Column(db.String(32), nullable=False)
+    before = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return f"<Teacher {self.t_id!r},{self.t_name!r}>"
+
+class Faces(db.Model):
+    __tablename__ = "student_faces"
+    s_id = db.Column(db.String(13), primary_key=True)
+    feature = db.Column(db.Text,nullable=False)
+
+    def __repr__(self):
+        return "<Faces %r>" % self.s_id
+
+
+class Course(db.Model):
+    __tablename__ = "courses"
+    c_id = db.Column(db.String(6), primary_key=True)
+    t_id = db.Column(db.String(8),db.ForeignKey("teachers.t_id"),nullable=False)
+    c_name = db.Column(db.String(100),nullable=False)
+    times = db.Column(db.Text,default="0000-00-00 00:00")
+    flag = db.Column(db.String(50), default="不可选课")
+
+    def __repr__(self):
+        return f"<Course {self.c_id!r},{self.t_id!r},{self.c_name!r}>"
+
+class SC(db.Model):
+    __tablename__ = "student_course"
+    s_id = db.Column(db.String(13),db.ForeignKey("students.s_id"),primary_key=True)
+    c_id = db.Column(db.String(100),db.ForeignKey("courses.c_id") ,primary_key=True)
+
+    def __repr__(self):
+        return f"<SC {self.s_id!r},{self.c_id!r}> "
+
+class Attendance(db.Model):
+    __tablename__ = "attendance"
+    id = db.Column(db.Integer,primary_key=True)
+    s_id = db.Column(db.String(13), db.ForeignKey("students.s_id"))
+    c_id = db.Column(db.String(100), db.ForeignKey("courses.c_id"))
+    time = db.Column(db.DateTime)
+    result = db.Column(db.String(10),nullable=False)
+
+    def __repr__(self):
+        return f"<Attendance {self.s_id!r},{self.c_id!r},{self.time!r},{self.result!r}>"
+
+class Time_id:
+    id = ""
+    time = ""
+
+    def __init__(self,id,time):
+        self.id = id
+        self.time = time
+
+class choose_course:
+    __tablename___ = "choose_course"
+    c_id = db.Column(db.String(6), primary_key=True)
+    t_id = db.Column(db.String(8), nullable=False)
+    c_name = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f"<Course {self.c_id!r},{self.t_id!r},{self.c_name!r}>"
