8f5ec041c555ba9c8e8776a5612198c8664fa9e1
BalconyJH <balconyjh@gmail.com>
2024-03-02 23:05:39 +0800

    :card_file_box: drop unused table

--- app/database/models.py
+++ app/database/models.py
@@ -3,26 +3,26 @@ from pydantic import BaseModel
 from app import db
 
 
-class User(db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(320), unique=True)
-    password = db.Column(db.String(32), nullable=False)
-
-    def __repr__(self):
-        return "<User %r>" % self.username
-
-
-class Admin(db.Model):
-    __tablename__ = "admins"
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(320), unique=True)
-    password = db.Column(db.String(32), nullable=False)
-
-    def __repr__(self):
-        return "<User %r>" % self.username
+# class User(db.Model):
+#     __tablename__ = "users"
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(80), unique=True)
+#     email = db.Column(db.String(320), unique=True)
+#     password = db.Column(db.String(32), nullable=False)
+#
+#     def __repr__(self):
+#         return "<User %r>" % self.username
+#
+#
+# class Admin(db.Model):
+#     __tablename__ = "admins"
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(80), unique=True)
+#     email = db.Column(db.String(320), unique=True)
+#     password = db.Column(db.String(32), nullable=False)
+#
+#     def __repr__(self):
+#         return "<User %r>" % self.username
 
 
 class Student(db.Model):
