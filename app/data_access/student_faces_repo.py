c87c7def3152589c6ac16cf52da58c8117c5e2d1
BalconyJH <balconyjh@gmail.com>
2024-03-08 01:11:30 +0800

    :rotating_light: lint

--- app/data_access/student_faces_repo.py
+++ app/data_access/student_faces_repo.py
@@ -3,5 +3,9 @@ from app.database.models import Faces
 
 class StudentFaces:
     @staticmethod
-    async def get_all_student_faces() -> list[Faces]:
+    def get_all_student_faces() -> list[Faces]:
+        """
+        获取所有学生人脸特征
+        :return: list[Faces]
+        """
         return Faces.query.all()
